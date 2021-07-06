3067f57832b6b6043738b5cad8bcefeb1c3161df
Magali Marques <maglynmarques@hotmail.com>
2021-07-06 13:09:47 -0300

    Resultado final

--- exerc.js
+++ exerc.js
@@ -4,8 +4,9 @@ let newStr ='';
 function reverseAString (str){ //comece com o comprimento , enquanto for >=0, decremento.
     for (let i= str.length -1; i>=0; i--){
         newStr += str [i]; // chamar a string atribuição newStr=newStr+str[i]
-        console.log(newStr);
+        
     }
+    console.log(newStr);
 }
 
 let resultado = reverseAString ('Hello Gama Academy')
