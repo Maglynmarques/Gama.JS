30ca60392ae19245cc429ac0a95449b887c7d768
Magali Marques <maglynmarques@hotmail.com>
2021-07-06 13:18:31 -0300

    Converter em Fahrenheit

--- exerc.js
+++ exerc.js
@@ -1,12 +1,8 @@
-// Reverse a String
-let newStr ='';
+// Convert Celsius to Fahrenheit
 
-function reverseAString (str){ //comece com o comprimento , enquanto for >=0, decremento.
-    for (let i= str.length -1; i>=0; i--){
-        newStr += str [i]; // chamar a string atribuição newStr=newStr+str[i]
-        
-    }
-    console.log(newStr);
+function convertToFahrenheit (value) {
+    return value * 1.8 + 32
 }
 
-let resultado = reverseAString ('Hello Gama Academy')
+let result = convertToFahrenheit(40)
+console.log(`O valor em Fahrenheit é ${result}`)
